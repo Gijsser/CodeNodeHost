8a9bb24927b5480edfcbfea4b26da6a605d7948d
GlennHoeks <55874566+GlennHoeks@users.noreply.github.com>
2020-06-02 11:07:17 +0200

    class data & Json added

---
+++ Host/Host/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+   public class Data
+    {
+        public int Id { get; set; }
+        public int SSN { get; set; }
+        public string Message { get; set; }
+
+        public Data()
+        {
+
+        }
+    }
+}
