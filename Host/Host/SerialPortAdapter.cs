b4e00b377aa719bf0051f3008c3a5151fbb6e2d8
g.hoeks@student.fontys.nl <i406645@fhict.nl>
2020-06-09 13:29:52 +0200

    arduino project

--- Host/Host/SerialPortAdapter.cs
+++ Host/Host/SerialPortAdapter.cs
@@ -9,7 +9,7 @@ namespace Host
         public SerialPortAdapter(SerialPort serialPort)
         {
             _serialPort = serialPort;
-            _serialPort.ReadTimeout = 10;//my serial port is connected to nothing and I get tired of waiting for failure.
+           // _serialPort.ReadTimeout = 10;//my serial port is connected to nothing and I get tired of waiting for failure.
         }
 
         virtual public string ReadLine()
